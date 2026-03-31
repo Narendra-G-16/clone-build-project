c4a08b943b94b139ff8ad6c7625fbf64ff03af4f
Narendra G <narendra@planetc.net>
2026-03-31 21:54:18 +0530

    Update greeting message in App.java

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Docker + Jenkins Pipeline!");
+        System.out.println("Hello Mounika! Project Done...");
     }
 }
