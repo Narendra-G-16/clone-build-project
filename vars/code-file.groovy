b72d7fc2cc54222568123946375f5bc3eb9b7ffc
Narendra G <narendra@planetc.net>
2026-03-31 21:30:45 +0530

    Change greeting message in main method

--- vars/code-file.groovy
+++ vars/code-file.groovy
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Shared Library Project!");
+        System.out.println("Hello from Docker + Jenkins Pipeline!");
     }
 }
